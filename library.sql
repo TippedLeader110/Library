bb62f93fbf13f39fa0a79fa18589dcf7c33344ba
TippedLeader110 <39187403+TippedLeader110@users.noreply.github.com>
2019-06-17 10:27:20 +0700

    Update library.sql

--- library.sql
+++ library.sql
@@ -1,100 +1,53 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for Win32 (AMD64)
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 14, 2019 at 10:04 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `library`
---
-
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `orang` (IN `human` TEXT)  begin
-select id, nama, alamat, no_telp from human;
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sel_branch` ()  begin
-select * from branch;
-end$$
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `tambah_buku` (IN `ISBN` VARCHAR(32), IN `ID_Penerbit` INT(8), IN `ID_Pengarang` INT(8), IN `thn_buku` INT(4), IN `jmlh` INT(4), IN `tgl_pengadaan` DATE, IN `Judul` VARCHAR(50))  begin
-insert into book values(ISBN, ID_Penerbit, ID_Pengarang, thn_buku, jmlh, tgl_pengadaan, Judul);
-end$$
-
---
--- Functions
---
-CREATE DEFINER=`root`@`localhost` FUNCTION `denda` (`date1` DATE, `id` INT(11), `id_trans` INT(11), `uang` INT(11)) RETURNS INT(11) begin
-declare date2 date;
-declare b int;
-Select t_deadline from pinjam_book where id_anggota = id and id_transaksi = id_trans into date2;
-if day(date1) > day(date2) then
-set b = (day(date1) - day(date2)) * uang;
-else
-set b = 0;
-end if;
-return b;
-end$$
-
-CREATE DEFINER=`root`@`localhost` FUNCTION `login` (`A` VARCHAR(32), `B` VARCHAR(32)) RETURNS INT(11) begin
-    declare cou int;
-
-SELECT COUNT(*) 
-into cou 
-from users
-where username = A 
-AND password = B;
-    
-    RETURN cou;
-    END$$
-
-DELIMITER ;
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `anggota`
 --
 
+DROP TABLE IF EXISTS `anggota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `anggota` (
   `id` int(11) NOT NULL,
   `nama` varchar(11) DEFAULT NULL,
   `alamat` text,
-  `no_telp` text
+  `no_telp` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `anggota`
 --
 
-INSERT INTO `anggota` (`id`, `nama`, `alamat`, `no_telp`) VALUES
-(1, 'bayhaqi', 'Goblogverse', '666'),
-(2, 'ihsan', ' budi', ' 9087'),
-(4, ' ali', ' patumbak', ' 09876');
-
--- --------------------------------------------------------
+LOCK TABLES `anggota` WRITE;
+/*!40000 ALTER TABLE `anggota` DISABLE KEYS */;
+INSERT INTO `anggota` VALUES (1,'bayhaqi','Goblogverse','666'),(2,'ihsan',' budi',' 9087'),(4,' ali',' patumbak',' 09876');
+/*!40000 ALTER TABLE `anggota` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `book`
 --
 
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `book` (
   `ISBN` varchar(32) NOT NULL,
   `Penerbit` text,
@@ -103,230 +56,256 @@ CREATE TABLE `book` (
   `jmlh` int(4) DEFAULT NULL,
   `tgl_pengadaan` date DEFAULT NULL,
   `Judul` varchar(50) DEFAULT NULL,
-  `lokasi` text NOT NULL
+  `lokasi` text NOT NULL,
+  PRIMARY KEY (`ISBN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`ISBN`, `Penerbit`, `Pengarang`, `thn_buku`, `jmlh`, `tgl_pengadaan`, `Judul`, `lokasi`) VALUES
-('1', '2', '1', 1, 1, '2019-06-14', '3', '1'),
-('2', ' wao', 'ihsan', 2019, 4, '2019-02-02', ' wao', 'disini'),
-('3', 'Milkita', 'Saiki Kusuo', 2019, 3, '2019-05-09', 'How to be a esper', 'rak a'),
-('4', 'TeaSeries', 'Pewdiepie', 2017, 5, '2019-06-11', 'Cross a bridge', 'Swedia'),
-('6', '10', '6', 2019, 6, '2019-06-13', '8', '6');
-
--- --------------------------------------------------------
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES ('1','2','1',1,1,'2019-06-14','3','1'),('2',' wao','ihsan',2019,4,'2019-02-02',' wao','disini'),('3','Milkita','Saiki Kusuo',2019,3,'2019-05-09','How to be a esper','rak a'),('4','TeaSeries','Pewdiepie',2017,5,'2019-06-11','Cross a bridge','Swedia'),('6','10','6',2019,6,'2019-06-13','8','6');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Stand-in structure for view `kembali`
--- (See below for the actual view)
+-- Temporary table structure for view `kembali`
 --
-CREATE TABLE `kembali` (
-`id_transaksi` int(11)
-,`isbn` varchar(32)
-,`anggota` varchar(11)
-,`petugas` varchar(11)
-,`judul` varchar(50)
-,`t_pinjam` date
-,`t_kembali` date
-,`t_deadline` date
-);
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `kembali`;
+/*!50001 DROP VIEW IF EXISTS `kembali`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `kembali` (
+  `id_transaksi` tinyint NOT NULL,
+  `isbn` tinyint NOT NULL,
+  `anggota` tinyint NOT NULL,
+  `petugas` tinyint NOT NULL,
+  `judul` tinyint NOT NULL,
+  `t_pinjam` tinyint NOT NULL,
+  `t_kembali` tinyint NOT NULL,
+  `t_deadline` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `lok_book`
 --
 
+DROP TABLE IF EXISTS `lok_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lok_book` (
   `id_rak` int(11) NOT NULL,
-  `lokasi` varchar(32) DEFAULT NULL
+  `lokasi` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id_rak`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Stand-in structure for view `pinjam`
--- (See below for the actual view)
+-- Dumping data for table `lok_book`
 --
-CREATE TABLE `pinjam` (
-`id_transaksi` int(11)
-,`nama` varchar(11)
-,`isbn` varchar(32)
-,`judul` varchar(50)
-,`t_pinjam` date
-,`t_kembali` date
-,`denda` int(8)
-);
 
--- --------------------------------------------------------
+LOCK TABLES `lok_book` WRITE;
+/*!40000 ALTER TABLE `lok_book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lok_book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary table structure for view `pinjam`
+--
+
+DROP TABLE IF EXISTS `pinjam`;
+/*!50001 DROP VIEW IF EXISTS `pinjam`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE TABLE `pinjam` (
+  `id_transaksi` tinyint NOT NULL,
+  `nama` tinyint NOT NULL,
+  `isbn` tinyint NOT NULL,
+  `judul` tinyint NOT NULL,
+  `t_pinjam` tinyint NOT NULL,
+  `t_kembali` tinyint NOT NULL,
+  `denda` tinyint NOT NULL
+) ENGINE=MyISAM */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Table structure for table `pinjam_book`
 --
 
+DROP TABLE IF EXISTS `pinjam_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pinjam_book` (
-  `id_transaksi` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL AUTO_INCREMENT,
   `id_anggota` int(11) NOT NULL,
   `id_petugas` int(11) NOT NULL,
   `ISBN` varchar(32) NOT NULL,
   `t_pinjam` date DEFAULT NULL,
   `t_deadline` date DEFAULT NULL,
   `t_kembali` date DEFAULT NULL,
-  `denda` int(8) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `denda` int(8) DEFAULT NULL,
+  PRIMARY KEY (`id_transaksi`),
+  KEY `id_anggota` (`id_anggota`),
+  KEY `ISBN` (`ISBN`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `pinjam_book`
 --
 
-INSERT INTO `pinjam_book` (`id_transaksi`, `id_anggota`, `id_petugas`, `ISBN`, `t_pinjam`, `t_deadline`, `t_kembali`, `denda`) VALUES
-(9, 2, 3, '1', '2019-06-11', '2019-06-14', '2019-06-13', 0),
-(10, 2, 3, '2', '2019-06-11', '2019-06-14', NULL, NULL),
-(11, 2, 3, '4', '2019-06-11', '2019-06-14', '2019-06-12', 0);
-
---
--- Triggers `pinjam_book`
---
-DELIMITER $$
-CREATE TRIGGER `peminjaman` AFTER INSERT ON `pinjam_book` FOR EACH ROW BEGIN
+LOCK TABLES `pinjam_book` WRITE;
+/*!40000 ALTER TABLE `pinjam_book` DISABLE KEYS */;
+INSERT INTO `pinjam_book` VALUES (9,2,3,'1','2019-06-11','2019-06-14','2019-06-13',0),(10,2,3,'2','2019-06-11','2019-06-14',NULL,NULL),(11,2,3,'4','2019-06-11','2019-06-14','2019-06-12',0);
+/*!40000 ALTER TABLE `pinjam_book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp932 */ ;
+/*!50003 SET character_set_results = cp932 */ ;
+/*!50003 SET collation_connection  = cp932_japanese_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER peminjaman after INSERT ON pinjam_book
+FOR EACH ROW BEGIN
 UPDATE book
 SET jumlah = jumlah - 1
 WHERE
 isbn = NEW.isbn;
-END
-$$
+END */;;
 DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `pengembalian` AFTER UPDATE ON `pinjam_book` FOR EACH ROW BEGIN
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = cp932 */ ;
+/*!50003 SET character_set_results = cp932 */ ;
+/*!50003 SET collation_connection  = cp932_japanese_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER pengembalian after update ON pinjam_book
+FOR EACH ROW BEGIN
 UPDATE book
 SET jumlah = jumlah + 1
 WHERE
 isbn = NEW.isbn;
-END
-$$
+END */;;
 DELIMITER ;
-
--- --------------------------------------------------------
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `reg_book`
 --
 
+DROP TABLE IF EXISTS `reg_book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `reg_book` (
   `no_regis` int(11) NOT NULL,
   `id_rak` int(11) NOT NULL,
-  `ISBN` varchar(32) DEFAULT NULL
+  `ISBN` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`no_regis`),
+  KEY `ISBN` (`ISBN`),
+  CONSTRAINT `reg_book_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `reg_book`
+--
+
+LOCK TABLES `reg_book` WRITE;
+/*!40000 ALTER TABLE `reg_book` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reg_book` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
   `nama` varchar(11) DEFAULT NULL,
   `alamat` text,
-  `no_telp` text
+  `no_telp` text,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `nama`, `alamat`, `no_telp`) VALUES
-(1, 'bayhaqi', '1', 'bayhaqi', NULL, NULL),
-(2, 'rafid', '2', 'rafid', 'lala', '666'),
-(3, 'ali', 'nigger', '  ali', ' patumbak', '  96');
-
--- --------------------------------------------------------
-
---
--- Structure for view `kembali`
---
-DROP TABLE IF EXISTS `kembali`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `kembali`  AS  select `d`.`id_transaksi` AS `id_transaksi`,`c`.`ISBN` AS `isbn`,`a`.`nama` AS `anggota`,`b`.`nama` AS `petugas`,`c`.`Judul` AS `judul`,`d`.`t_pinjam` AS `t_pinjam`,`d`.`t_kembali` AS `t_kembali`,`d`.`t_deadline` AS `t_deadline` from (((`pinjam_book` `d` join `anggota` `a` on((`a`.`id` = `d`.`id_anggota`))) join `users` `b` on((`b`.`id` = `d`.`id_petugas`))) join `book` `c` on((`c`.`ISBN` = `d`.`ISBN`))) ;
-
--- --------------------------------------------------------
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'bayhaqi','1','bayhaqi',NULL,NULL),(2,'rafid','2','rafid','lala','666'),(3,'ali','nigger','  ali',' patumbak','  96');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure for view `pinjam`
---
-DROP TABLE IF EXISTS `pinjam`;
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `pinjam`  AS  select `a`.`id_transaksi` AS `id_transaksi`,`b`.`nama` AS `nama`,`a`.`ISBN` AS `isbn`,`c`.`Judul` AS `judul`,`a`.`t_pinjam` AS `t_pinjam`,`a`.`t_kembali` AS `t_kembali`,`a`.`denda` AS `denda` from ((`pinjam_book` `a` join `anggota` `b` on((`a`.`id_anggota` = `b`.`id`))) join `book` `c` on((`a`.`ISBN` = `c`.`ISBN`))) ;
-
---
--- Indexes for dumped tables
+-- Final view structure for view `kembali`
 --
 
---
--- Indexes for table `anggota`
---
-ALTER TABLE `anggota`
-  ADD PRIMARY KEY (`id`);
+/*!50001 DROP TABLE IF EXISTS `kembali`*/;
+/*!50001 DROP VIEW IF EXISTS `kembali`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `kembali` AS select `d`.`id_transaksi` AS `id_transaksi`,`c`.`ISBN` AS `isbn`,`a`.`nama` AS `anggota`,`b`.`nama` AS `petugas`,`c`.`Judul` AS `judul`,`d`.`t_pinjam` AS `t_pinjam`,`d`.`t_kembali` AS `t_kembali`,`d`.`t_deadline` AS `t_deadline` from (((`pinjam_book` `d` join `anggota` `a` on((`a`.`id` = `d`.`id_anggota`))) join `users` `b` on((`b`.`id` = `d`.`id_petugas`))) join `book` `c` on((`c`.`ISBN` = `d`.`ISBN`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 
 --
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`ISBN`);
-
---
--- Indexes for table `lok_book`
---
-ALTER TABLE `lok_book`
-  ADD PRIMARY KEY (`id_rak`);
-
---
--- Indexes for table `pinjam_book`
---
-ALTER TABLE `pinjam_book`
-  ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `id_anggota` (`id_anggota`),
-  ADD KEY `ISBN` (`ISBN`);
-
---
--- Indexes for table `reg_book`
---
-ALTER TABLE `reg_book`
-  ADD PRIMARY KEY (`no_regis`),
-  ADD KEY `ISBN` (`ISBN`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Final view structure for view `pinjam`
 --
 
---
--- AUTO_INCREMENT for table `pinjam_book`
---
-ALTER TABLE `pinjam_book`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+/*!50001 DROP TABLE IF EXISTS `pinjam`*/;
+/*!50001 DROP VIEW IF EXISTS `pinjam`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `pinjam` AS select `a`.`id_transaksi` AS `id_transaksi`,`b`.`nama` AS `nama`,`a`.`ISBN` AS `isbn`,`c`.`Judul` AS `judul`,`a`.`t_pinjam` AS `t_pinjam`,`a`.`t_kembali` AS `t_kembali`,`a`.`denda` AS `denda` from ((`pinjam_book` `a` join `anggota` `b` on((`a`.`id_anggota` = `b`.`id`))) join `book` `c` on((`a`.`ISBN` = `c`.`ISBN`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `reg_book`
---
-ALTER TABLE `reg_book`
-  ADD CONSTRAINT `reg_book_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-06-17 10:26:47
